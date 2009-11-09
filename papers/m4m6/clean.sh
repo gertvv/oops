62ee6d53eee7774ac62ae6f4af266d8ac1bfcd8f
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2009-11-09 14:11:56 +0100

    Infrastructure for presentation

--- papers/m4m6/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -f *.aux *.bbl *.blg *.log *.out *.spl *.pdf
--- papers/m4m6/package.sh
+++ papers/m4m6/package.sh
@@ -14,3 +14,6 @@ cp ../styles/entcs/entcs.cls $DEST/
 cp ../styles/entcs/entcs.bst $DEST/
 cp lua.def $DEST/
 cp paper.pdf $DEST/
+
+rm -f m4m6-oops.zip
+zip -r m4m6-oops $DEST/
